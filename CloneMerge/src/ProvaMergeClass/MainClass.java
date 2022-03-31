f4ef5e115a09338057f40ffce46b803986995f2e
jaume <jaume@DESKTOP-7VE5B1S>
2022-03-31 17:16:01 +0200

    Inicial 3

---
+++ CloneMerge/src/ProvaMergeClass/MainClass.java
@@ -0,0 +1,15 @@
+package ProvaMergeClass;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        //Activitat Control versions 2
+        
+        System.out.println("A");
+        
+        System.out.println("B");
+        
+        System.out.println("C");
+    }
+    
+}
